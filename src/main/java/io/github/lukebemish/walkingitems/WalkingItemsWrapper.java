fb08944bacde379c8912f0bed31723cded5c3df8
Luke Bemish <lukebemish@gmail.com>
2022-07-03 19:14:18 -0500

    Update WalkingItemsWrapper.java

--- src/main/java/io/github/lukebemish/walkingitems/WalkingItemsWrapper.java
+++ src/main/java/io/github/lukebemish/walkingitems/WalkingItemsWrapper.java
@@ -2,10 +2,10 @@ package io.github.lukebemish.walkingitems;
 
 import net.minecraftforge.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(WalkingItems.MODID)
 public class WalkingItemsWrapper {
     public WalkingItemsWrapper() {
+        //noinspection InstantiationOfUtilityClass
         new WalkingItems();
     }
 }
